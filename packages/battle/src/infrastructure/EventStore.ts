ccc7bacc431dd282b0a51d39e5dce869ea9f5bfd
Alexandre Martinez <alexandre.martinez76@gmail.com>
2018-07-19 15:33:49 +0200

    Store OK

---
+++ packages/battle/src/infrastructure/EventStore.ts
@@ -0,0 +1,23 @@
+export class EventDontContainsAggregateId implements Error {
+    public eventName: string;
+    constructor(eventName) {
+        this.eventName = eventName;
+    }
+}
+
+export class EventStore {
+    public events: any[] = [];
+
+    public store(event) {
+        if (!event.getAggregateId) {
+            throw new EventDontContainsAggregateId(event.constructor.name);
+        }
+        this.events.push(event);
+    }
+
+    public getEventsOfAggregate(aggregateId) {
+        return this.events.filter((event) => {
+            return event.getAggregateId().equals(aggregateId);
+        });
+    }
+}
